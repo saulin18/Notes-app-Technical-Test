b581e3a524e3fd535d1d3f5fd46a6a6e528c1d56
saul <sondonsaul@gmail.com>
2025-01-25 19:58:39 -0800

    [fix] Readme.md and run.sh, frontend dockerfile

--- frontend/src/api/notes.ts
+++ frontend/src/api/notes.ts
@@ -41,15 +41,6 @@ export const deleteNoteRequest = async (id: number) => {
   await axi.put(`/notes/${id}/delete/`);
 };
 
-export const getActiveNotesRequest: QueryFunction<Note[]> = async () => {
-  const response = await axi.get("/notes/active/");
-  return response.data;
-};
-
-export const getArchivedNotesRequest: QueryFunction<Note[]> = async () => {
-  const response = await axi.get("/notes/inactive/");
-  return response.data;
-};
 
 export const archiveNoteRequest = async (id: number) => {
   await axi.put(`/notes/${id}/archive/`);
